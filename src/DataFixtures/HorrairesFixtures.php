9603f6c9092b50058ac6fcbfb81a959fe065d904
kryweb <dreidemyromain@gmail.com>
2020-01-16 17:06:04 +0100

    Correction de la fixtures Horraires

--- src/DataFixtures/HorrairesFixtures.php
+++ src/DataFixtures/HorrairesFixtures.php
@@ -9,7 +9,7 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class SpeciliteToCdsixtures extends Fixture implements DependentFixtureInterface
+class HorrairesFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
