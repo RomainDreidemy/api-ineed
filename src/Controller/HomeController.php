a294218a818f458b91e0957123dac01b7042c8c4
kryweb <dreidemyromain@gmail.com>
2020-01-16 10:59:50 +0100

    Création de la fixture Hopitaux + ajout des fichiers de datas

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,10 +8,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
     public function index()
     {
+        $csv = file_get_contents(__DIR__ . '/../../public/datas/hopitaux.csv');
+
+        $hopitaux = explode("\r\n", $csv);
+
+        foreach ($hopitaux as $h){
+            $h = explode(';', $h);
+            $h[3] = str_replace(' ', '', $h[3]);
+            dump($h);
+        }
+        dd();
 
 
         return $this->render('home/index.html.twig', [
---
+++ src/DataFixtures/HopitauxFixtures.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Arrondissement;
+use App\Entity\Hopital;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class HopitauxFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $csv = file_get_contents(__DIR__ . '/../../public/datas/hopitaux.csv');
+
+        $hopitaux = explode("\r\n", $csv);
+
+        foreach ($hopitaux as $h){
+            $h = explode(';', $h);
+            $h[3] = str_replace(' ', '', $h[3]);
+
+            $hopital = new Hopital();
+
+            $hopital
+                ->setName($h[0])
+                ->setAddress($h[1])
+                ->setTelephone($h[3])
+                ->setArrondissement(
+                    $manager->getRepository(Arrondissement::class)->findOneBy(
+                        [
+                            'postal_code' => $h[2]
+                        ]
+                    )
+                )
+            ;
+             $manager->persist($hopital);
+        }
+
+        $manager->flush();
+    }
+}
