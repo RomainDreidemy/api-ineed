942c4f940eb8feb0590d58d417bcb9ba4c552603
kryweb <dreidemyromain@gmail.com>
2020-02-11 16:20:26 +0100

    Inscription d'un utilisateur

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,13 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
+    private $encoder;
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+
     /**
      * @Route("/login", name="login", methods={"POST"})
      */
@@ -21,5 +31,38 @@ class SecurityController extends AbstractController
             'username' => $user->getUsername(),
             'roles' => $user->getRoles(),
         ]);
+    }
+
+    /**
+     * @Route("/users/add", name="addUser", methods={"POST"})
+     */
+    public function userAdd(Request $request, EntityManagerInterface $entityManager)
+    {
+        if($_SERVER['REQUEST_METHOD']==='POST' && empty($_POST)) {
+            $_POST = json_decode($request->getContent(), true);
+        }
+
+        $is_exist_user = $entityManager->getRepository(User::class)->findOneBy(['email' => $_POST['email']]);
+
+        if(is_null($is_exist_user)){
+
+            $user = new User();
+
+            $user
+                ->setEmail($_POST['email'])
+                ->setPassword($this->encoder->encodePassword($user, $_POST['password']))
+            ;
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->json([
+                'user' => $user
+            ]);
+        }else{
+            return $this->json([
+                'error' => 'Email already use'
+            ], 500);
+        }
     }
 }
