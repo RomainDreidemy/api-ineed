58762b854923a7bf7d16c39b02bbd3a4e0cc6bcb
kryweb <dreidemyromain@gmail.com>
2020-02-26 19:57:38 +0100

    Backoffice - CRUD Hopital

--- src/Controller/Backoffice/HopitalController.php
+++ src/Controller/Backoffice/HopitalController.php
@@ -3,8 +3,10 @@
 namespace App\Controller\Backoffice;
 
 use App\Entity\Hopital;
+use App\Form\HopitalType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HopitalController extends AbstractController
@@ -18,4 +20,69 @@ class HopitalController extends AbstractController
             'hopitaux' => $entityManager->getRepository(Hopital::class)->findBy([], ['name' => 'ASC']),
         ]);
     }
+
+    /**
+     * @Route("/administration/hopital/add", name="hopital_add")
+     */
+    public function addHopital(EntityManagerInterface $entityManager, Request $request)
+    {
+        $form = $this->createForm(HopitalType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $data = $form->getData();
+            $entityManager->persist($data);
+            $entityManager->flush();
+            $this->addFlash('success', 'The new Hospital have been added');
+            return $this->redirectToRoute('hopital');
+        }
+
+        return $this->render('hopital/add-update.html.twig', [
+            'form' => $form->createView(),
+            'title' => 'Add a hospital'
+        ]);
+    }
+
+    /**
+     * @Route("/administration/hopital/update/{id}", name="hopital_update")
+     */
+    public function updateHopital(Hopital $hopital,EntityManagerInterface $entityManager, Request $request)
+    {
+        $form = $this->createForm(HopitalType::class, $hopital);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $data = $form->getData();
+            $entityManager->persist($data);
+            $entityManager->flush();
+            $this->addFlash('success', 'The Hospital have been updated');
+            return $this->redirectToRoute('hopital');
+        }
+
+        return $this->render('hopital/add-update.html.twig', [
+            'form' => $form->createView(),
+            'title' => 'Update a hospital'
+        ]);
+    }
+
+    /**
+     * @Route("/administration/hopital/remove/{id}", name="hopital_remove")
+     */
+    public function removeHopital(Hopital $hopital,EntityManagerInterface $entityManager)
+    {
+        $entityManager->remove($hopital);
+        $entityManager->flush();
+        $this->addFlash('success', 'The Hospital have been removed');
+        return $this->redirectToRoute('hopital');
+    }
+    /**
+     * @Route("/administration/hopital/see/{id}", name="hopital_see")
+     */
+    public function seeHopital(Hopital $hopital,EntityManagerInterface $entityManager)
+    {
+        return $this->render('hopital/see.html.twig', [
+            'hopital' => $hopital
+        ]);
+    }
+
 }
---
+++ src/Form/HopitalType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Hopital;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class HopitalType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('address')
+            ->add('telephone')
+            ->add('longitude')
+            ->add('latitude')
+            ->add('Arrondissement')
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Hopital::class,
+        ]);
+    }
+}
