ad61317d59620bcb4babbe6d1eb1c868b6c6198f
kryweb <dreidemyromain@gmail.com>
2020-02-26 19:37:21 +0100

    Redirection / => /api

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -20,26 +20,6 @@ class HomeController extends AbstractController
      */
     public function index(EntityManagerInterface $entityManager)
     {
-        $pharmacies = json_decode(file_get_contents(__DIR__ . '/../../public/datas/pharmacie-heure.json'));
-
-        foreach ($pharmacies->data->data->member as $p){
-
-            foreach ($p->weekday_text as $item){
-                $horraires = explode(" ", $item);
-                dd($horraires);
-
-                if(sizeof($horraires) !== 6){
-                    dump(substr($horraires[0], 0, -1));
-                    dd($horraires);
-                }
-            }
-
-        }
-
-        return $this->json([
-           'Projet' => 'I need (API)',
-            'Développeur' => 'Romain Dreidemy',
-            'Auth' => $this->getUser() ? true : false
-        ]);
+        return $this->redirectToRoute('api_entrypoint');
     }
 }
